681856b23572d18f30a55b30ef7095b3aeefa714
thomas jerez <tjerez@outlook.com>
2022-09-05 12:44:53 -0300

    agregue funcionalidad de las animaciones de la pagina de detalles

---
+++ js/detalle.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel)=> {
+    panel.addEventListener("click", ()=> {
+        removerClases();
+        panel.classList.add("active");
+    });
+});
+
+const removerClases = () => {
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+    });
+};
