5632fa227b24e6e330b06b69c4215429f50e0af9
thomas jerez <tjerez@outlook.com>
2022-09-05 14:24:41 -0300

    agregue las validaciones del formulario

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const modalFormLogin = new bootstrap.Modal(document.querySelector('#modalLogin')
 
 function mostrarModal(){
     modalFormLogin.show();    
-
+    limpiarModal();
 }
 
 const btnLogin =document.querySelector('#btnModal')
@@ -15,13 +15,21 @@ let email = document.querySelector('#email')
 let contrasenia = document.querySelector('#contraseña')
 
 btnLogin.addEventListener('click', mostrarModal);
-formulario.addEventListener('submit');
+formulario.addEventListener('submit', crearUsuario);
+nombre.addEventListener('blur', ()=>{validarNombre(nombre)})
+email.addEventListener('blur', ()=>{validarEmail(email)})
+contrasenia.addEventListener('blur', ()=>{validarContrasenia(contrasenia)})
 
-
-
+function crearUsuario(e){
+    e.preventDefault();
+  
+}
 
 function limpiarModal(){
-
+    formulario.reset();
+    nombre.className = 'form-control'
+    email.className = 'form-control'
+    contrasenia.className = 'form-control'
 }
 
 
