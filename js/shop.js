83ec0274c669c80a8e831e9a7621562cf80bdce2
Tomas <tomibudeguer20@gmail.com>
2022-09-05 19:57:09 -0300

    hice la segunda parte de la funcion del usuario admnin en login

--- js/shop.js
+++ js/shop.js
@@ -1,11 +1,19 @@
 let listaProductos =
   JSON.parse(localStorage.getItem("listaProductosKey")) || [];
 
-// let listaUsuarios = JSON.parse(localStorage.getItem("listaPeliculasKey")) || [];
+let listaUsuarios = JSON.parse(localStorage.getItem("listaUsuariosKey")) || [];
 
-// let navAdmin = document.querySelector("#navAdmin");
-// let etiquetaAdmin = `<a href="admin.html" class="nav-link">Administrador</a>`;
-// navAdmin.innerHTML += etiquetaAdmin;
+for (let i = 0; i < listaUsuarios.length; i++) {
+  if (
+    listaUsuarios[i].nombre === "Administrador" &&
+    listaUsuarios[i].contrasenia === "Lujenntho1" &&
+    listaUsuarios[i].email === "administrador@lujenntho.com"
+  ) {
+    let navAdmin = document.querySelector("#navAdmin");
+    let etiquetaAdmin = `<a href="admin.html" class="nav-link">Administrador</a>`;
+    navAdmin.innerHTML = etiquetaAdmin;
+  }
+}
 
 listaProductos.map((producto) => {
   hacerGrilla(producto);
