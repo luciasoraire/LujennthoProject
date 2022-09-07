0aac03669d75c90ac20a68926ab6a00c38d5439e
Tomas <tomibudeguer20@gmail.com>
2022-09-07 01:33:33 -0300

    agregue la funcion para que se creen las etiquetas del admin cuando se registre de forma correcta en todos los archivos

--- js/admin.js
+++ js/admin.js
@@ -7,6 +7,19 @@ import {
   validarImagen,
   validarPrecio,
 } from "./helpers.js";
+let listaUsuarios = JSON.parse(localStorage.getItem("listaUsuariosKey")) || [];
+
+for (let i = 0; i < listaUsuarios.length; i++) {
+  if (
+    listaUsuarios[i].nombre === "Administrador" &&
+    listaUsuarios[i].contrasenia === "Lujenntho1" &&
+    listaUsuarios[i].email === "administrador@lujenntho.com"
+  ) {
+    let navAdmin = document.querySelector("#navAdmin");
+    let etiquetaAdmin = `<a href="admin.html" class="nav-link">Administrador</a>`;
+    navAdmin.innerHTML = etiquetaAdmin;
+  }
+}
 
 let formAdmin = document.querySelector("#formAdmin");
 let codigo = document.querySelector("#codigo");
--- js/login.js
+++ js/login.js
@@ -82,3 +82,15 @@ function limpiarModal() {
   email.className = "form-control";
   contrasenia.className = "form-control";
 }
+
+for (let i = 0; i < listaUsuarios.length; i++) {
+  if (
+    listaUsuarios[i].nombre === "Administrador" &&
+    listaUsuarios[i].contrasenia === "Lujenntho1" &&
+    listaUsuarios[i].email === "administrador@lujenntho.com"
+  ) {
+    let navAdmin = document.querySelector("#navAdmin");
+    let etiquetaAdmin = `<a href="pages/admin.html" class="nav-link">Administrador</a>`;
+    navAdmin.innerHTML = etiquetaAdmin;
+  }
+}
